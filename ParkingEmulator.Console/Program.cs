e91b1ec5a9c606136c16a2614b423086ebecc097
Mykola Kobets <nicko9298@gmail.com>
2018-05-14 11:13:27 +0300

    Initial commit. Created WebAPI project, imported ParkingEmulator.Core and ParkingEmulator.Log from last repository

---
+++ ParkingEmulator.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using ParkingEmulator.Console.Presentation;
+using ParkingEmulator.Core.Kernel;
+using ParkingEmulator.Log.Logging;
+
+namespace ParkingEmulator.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parking = Parking.GetInstance;
+
+            TransactionLogger.LogInit();
+            Navigation.RunMenu(parking); 
+        }
+    }
+}
